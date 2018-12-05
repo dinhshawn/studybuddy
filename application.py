4ee2215ca7b566dbafb7b9a2c5a0c2e60ffbe96f
Shawn Dinh <shawndinh@college.harvard.edu>
2018-12-05 04:09:19 +0000

    tenth commit

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required
